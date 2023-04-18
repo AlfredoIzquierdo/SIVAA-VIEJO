73749cc788f52aaa18be3b8fe8bc25667657b54c
AlfredoIzquierdo <alfredoizquierdo111@gmail.com>
2023-04-18 13:24:58 -0700

    asdasd

--- Pantallas SIVAA/CarpetaProovedores/ProovedoresPrin.cs
+++ Pantallas SIVAA/CarpetaProovedores/ProovedoresPrin.cs
@@ -24,6 +24,7 @@ namespace Pantallas_SIVAA
         // prueba 4
         //prueba 5
         //prueba 6
+        //prueba 7
         public ProovedoresPrin()
         {
             InitializeComponent();
